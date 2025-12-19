2fc94ad6e0ee6ab59d6a4d459509eba4a98460a7
BlinkyStitt <bryan@stitthappens.com>
2025-12-19 07:19:42 +0000

    Deploying to gh-pages from @ SatoshiAndKin/farcaster-forest@08115d7fe79bff4e467f71ada75c2740d59946be 🚀

--- bevy_game-196bac9bb1360ce4.js
+++ bevy_game-196bac9bb1360ce4.js
@@ -325,16 +325,16 @@ function __wasm_bindgen_func_elem_85727(arg0, arg1, arg2) {
     wasm.__wasm_bindgen_func_elem_85727(arg0, arg1, addHeapObject(arg2));
 }
 
-function __wasm_bindgen_func_elem_88430(arg0, arg1, arg2) {
-    wasm.__wasm_bindgen_func_elem_88430(arg0, arg1, isLikeNone(arg2) ? 0 : addHeapObject(arg2));
-}
-
 function __wasm_bindgen_func_elem_81821(arg0, arg1, arg2, arg3) {
     wasm.__wasm_bindgen_func_elem_81821(arg0, arg1, addHeapObject(arg2), addHeapObject(arg3));
 }
 
 function __wasm_bindgen_func_elem_82198(arg0, arg1) {
     wasm.__wasm_bindgen_func_elem_82198(arg0, arg1);
+}
+
+function __wasm_bindgen_func_elem_88430(arg0, arg1, arg2) {
+    wasm.__wasm_bindgen_func_elem_88430(arg0, arg1, isLikeNone(arg2) ? 0 : addHeapObject(arg2));
 }
 
 const __wbindgen_enum_GamepadMappingType = ["", "standard"];
